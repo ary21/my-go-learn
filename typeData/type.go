dee6bb15ad46e6d079888e4bfcccea1a0344397b
ary21 <aryjamers.21@gmail.com>
2024-06-16 22:01:16 +0700

    my commit

---
+++ typeData/type.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("num", 1)
+	fmt.Println("dec", 1.4)
+	fmt.Println("bool", true, false)
+	fmt.Println(len("hello"))
+	fmt.Println("hellp"[2])
+}
